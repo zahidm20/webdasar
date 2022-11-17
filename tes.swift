0ac7ebde025d468f629ccd1f2420d26af37dbaf2
zahidm20 <zahidmaulana20@gmail.com>
2022-11-17 10:00:00 +0700

    first commit

---
+++ tes.swift
@@ -0,0 +1,15 @@
+import swiftui
+
+struct ContenView: View {
+    var body:some View {
+text("hello swiftui!")
+.padding()
+    }
+}
+
+struct ContentView_previews: { PreviewProdiver {
+    static var previews: some View {
+        contentview()
+    }
+}   
+}
